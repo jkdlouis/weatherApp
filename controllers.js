39aada5b2c79cc5f6861db305f26fee4b601341b
Louis Huang <0563louishuang@gmail.com>
2016-08-18 13:03:23 -0700

    custom service, cleaner codes

--- controllers.js
+++ controllers.js
@@ -1,23 +1,25 @@
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
     
     $scope.city = cityService.city;
     
     $scope.$watch('city', function() {
        cityService.city = $scope.city; 
     });
+
+    $scope.submit = function() {
+       $location.path("/forecast");
+    };
     
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService', function($scope, $routeParams, cityService, weatherService) {
     
     $scope.city = cityService.city;
     
     $scope.days = $routeParams.days || '2';
-    
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=ee0098e80ea0c24f703c96584f5d6005", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
-    
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+
+    $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
     
     $scope.convertToFahrenheit = function(degK) {
         
--- services.js
+++ services.js
@@ -4,3 +4,21 @@ weatherApp.service('cityService', function() {
     this.city = "New York, NY";
     
 });
+
+weatherApp.service('weatherService', ['$resource', function($resource) {
+
+    this.GetWeather = function(city, days) {
+
+        var weatherAPI =
+            $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=ee0098e80ea0c24f703c96584f5d6005",
+                {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
+
+        return weatherAPI.get({
+            q: city, cnt: days
+        });
+
+
+    };
+
+
+}]);
